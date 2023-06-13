fe8751c9e8bd69d55e526ac2d1f7218fe878a621
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2023-06-13 08:13:26 +0100

    adding slack functionality

--- src/server.js
+++ src/server.js
@@ -10,8 +10,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/.netlify/functions/server", router);
 
+const format = (answers) => {
+  if (answers.length == 0) {
+    answers = ["No answers found"];
+  }
+
+  let formatted = {
+    blocks: [],
+  };
+
+  for (answer of answers) {
+    formatted["blocks"].push({
+      type: "divider",
+    });
+    formatted["blocks"].push({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: answer,
+      },
+    });
+  }
+
+  return formatted;
+};
+
 router.get("/test", (req, res) => {
   res.json({ hello: "world" });
+});
+
+router.post("/", (req, res) => {
+  const text = req.body.text;
+  let answers = text;
+  const formattedAnswers = format(answers);
+  console.log(`Input text: ${text}`);
+  res.status(200);
+  res.send(formattedAnswers);
 });
 
 module.exports.handler = serverless(app);
